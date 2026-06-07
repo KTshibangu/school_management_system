91a5d8cd6722691433c904199e0521c9dab6892a
KTshibangu <roddykamba@gmail.com>
2026-06-07 19:31:51 +0200

    first commit

---
+++ client/src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowseRouter } from 'react-router-dom'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <BrowseRouter>
+    <App />
+  </BrowseRouter>,
+)
