83362df8640373b0ed9d25d6c084ac79d0f22c35
KTshibangu <ktshibangu@gmail.com>
2026-07-27 18:04:53 +0200

    Modified Landing Page

--- client/src/components/LoginLeftSide.jsx
+++ client/src/components/LoginLeftSide.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
+import Badge from '../assets/badge.png'
 
 const LoginLeftSide = () => {
   return (
-    <div className="hidden md:flex w-1/2 bg-slate-700 relative overflow-hidden border-r border-slate-200">
+    <div className="hidden md:flex w-1/2 bg-white overflow-hidden border-r border-slate-200">
       <div className="absolute -top-32 -left-32 w-72 h-72 bg-indigo-100/20 rounded-full blur-3xl"></div>
 
-      <div className="relative z-10 flex flex-col items-start justify-center p-12 lg:p-20 w-full h-full">
-        <h1 className="text-4xl lg:text-5xl font-medium text-white mb-6 leading-tight tracking-tight">
-          School <br /> Management System
+      <div className="relative z-10 flex flex-col items-center justify-center p-12 lg:p-20 w-full h-full">
+        <h1 className="text-4xl lg:text-5xl font-medium text-slate-900 mb-6 leading-tight tracking-tight">
+          School Management System
         </h1>
-        <p className="text-white text-lg max-w-md leading-relaxed">
-          Streamline your school operations, organize classes, teachers, tasks
-          and school events
+        <h2 className="text-4xl lg:text-4xl font-medium text-slate-900 mb-6 leading-tight tracking-tight">
+          Example High School
+        </h2>
+        <img src={Badge} className='w-100' />
+        <p className="text-slate-900 text-lg max-w-md leading-relaxed">
+          For testing admin portal:
+        </p>
+        <p className="text-slate-900 text-sm max-w-md leading-relaxed">
+          admin@schoolted.com / admin123
+        </p>
+        <p className="text-slate-900 text-lg max-w-md leading-relaxed">
+          For testing teacher portal:
+        </p>
+        <p className="text-slate-900 text-sm max-w-md leading-relaxed">
+          ktshibangu@schoolted.com / 12345678
         </p>
       </div>
     </div>
